3668c28df90601855a35ff3a7eb1558aeb919051
Piorosen <aoikazto@gmail.com>
2022-12-13 00:00:12 +0900

    add bootstrap files

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt install unzip
+
+git submodule update --init --recursive
+
+mkdir components
+
+wget "https://dl.google.com/android/repository/android-ndk-r25b-linux.zip?utm_source=developer.android.com&utm_medium=referral" -O components/android-ndk-r25b.zip
+
+tar -zxvf components/android-ndk-r25b.zip -P components
+
+echo "export PATH=$(pwd)/components/android-ndk-r25b/toolchains/llvm/prebuilt/linux-x86_64/bin:$$PATH" >> ~/.bashrc
